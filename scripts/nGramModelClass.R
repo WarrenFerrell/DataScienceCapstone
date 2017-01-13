c2a8f14ac98d0cad07ac697b0e3bcb3f47dc53e0
Warren Ferrell <warren.ferrell@colorado.edu>
2017-01-12 17:04:54 -0700

    Need to fix how much time being spent in gc function

--- scripts/nGramModelClass.R
+++ scripts/nGramModelClass.R
@@ -2,9 +2,10 @@ library(pryr)
 
 nGramModel <- function(model = new.env(), size = mem_used() * 0,
                        maxMem = 100, minFreq = 2,
-                       maxGram = 10){
+                       maxGram = 10, timesCleaned=0){
     attr <- list(model=model,size=size,maxMem=maxMem,
-                 minFreq=minFreq,maxGram=maxGram)
+                 minFreq=minFreq,maxGram=maxGram,
+                 timesCleaned=timesCleaned)
     structure(attr, class = 'nGramModel')
 }
 
